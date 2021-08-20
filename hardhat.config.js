730e6b133ff5de5a25f8807148902d89b8e77770
sam bacha <sam@manifoldfinance.com>
2021-08-20 04:46:58 -0700

    solc output

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,52 @@
-require("@nomiclabs/hardhat-waffle");
-require("hardhat-tracer");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+// hardhat.config.js
+// const { alchemyApiKey, mnemonic } = require('./secrets.json');
+require('@nomiclabs/hardhat-ethers');
+require("@typechain/hardhat");
+require("hardhat-packager");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+/*    networks: {
+        rinkeby: {
+            url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
+            accounts: {
+                mnemonic: mnemonic
+            }
+        }
+    },*/
+    packager: {
+      // What contracts to keep the artifacts and the bindings for.
+      contracts: ["BeeeefRegistry"],
+      // Whether to include the TypeChain factories or not.
+      // If this is enabled, you need to run the TypeChain files through the TypeScript compiler before shipping to the registry.
+      includeFactories: true,
+    },
+    paths: {
+      artifacts: "./artifacts",
+      cache: "./cache",
+      sources: "./contracts",
+ //     tests: "./test",
+    },
+    solidity: {
+        version: '0.8.4',
+        settings: {
+            metadata: {
+                bytecodeHash: 'none',
+            },
+            optimizer: {
+                enabled: false,
+                runs: 200,
+                details: {
+                    yul: false,
+                },
+            },
+        },
+        typechain: {
+          outDir: "typechain",
+          target: "ethers-v5",
+        },
+    }
+    
 };
